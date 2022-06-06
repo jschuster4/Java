d06a35840a4769442a730c4ba29b02df27d0271b
jschuster4 <jspowerex@gmail.com>
2022-06-06 18:15:19 -0400

    initial commit

---
+++ fundamentals/first_java_program/Own.java
@@ -0,0 +1,8 @@
+public class Own {
+
+    public static void main(String[] args){
+        System.out.println("My name is Joel Schuster");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Dublin, OH");
+    }
+}
